e7b7f191a43b8604ee2632ac2f07c7a26b8851d6
Kalpit Soni <31398432+sonikalpit@users.noreply.github.com>
2020-11-09 21:36:23 +0530

    Version 1.0

--- TrignoFunctions/src/TrigoTaylorFunction.java
+++ TrignoFunctions/src/TrigoTaylorFunction.java
@@ -1,4 +1,44 @@
+/*
+ *This class is responsible for computing Sin, Cos, Tan functions by implementing required functions such
+ *as Power function, Factorial functions which are required for implementing Taylor series.
+ *
+ *@author Team 11 - Azmina, Nithya, Jigish, Kalpit
+ *@version 1.0
+ *
+ */
+
+import java.util.Scanner;
 
 public class TrigoTaylorFunction {
+	
+	// Conversion of Degree input to Radian
+	public static double DegToRad(double deg1)
+	{
+		
+	}
+	// Normalizing radian to make input calculatable
+	public static double RadNormalize(double rad1)
+	{
+		
+	}
+	public static double powerResult(double base,double power)
+	{
+		
+	}
+	public static long Factorial(int den)
+	{
+		
+	}
+	public static double TrignoSin(double radinput)
+	{
+		/*
+		 * taylor series for sin is summation of (-1)n* x(2n+1)/ fact(2n+1)
+		 * */
+		
+	}
+	public static double TrignoCos()
+	{
+		
+  }
 
 }
