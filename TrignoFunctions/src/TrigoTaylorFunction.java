1b1ac582c81efe67f96e078eec716bccfb6f1e86
Kalpit Soni <31398432+sonikalpit@users.noreply.github.com>
2020-11-10 12:18:30 +0530

    Version 2.0
    Verison 2.0 has many implementation to execute Sin function such as, calculating factorial, power, Degree conversion and Sin implementation as well that are mentioned in Taylor series

--- TrignoFunctions/src/TrigoTaylorFunction.java
+++ TrignoFunctions/src/TrigoTaylorFunction.java
@@ -14,27 +14,80 @@ public class TrigoTaylorFunction {
 	// Conversion of Degree input to Radian
 	public static double DegToRad(double deg1)
 	{
-		
+		return (2*deg1*pi)/angle;
 	}
 	// Normalizing radian to make input calculatable
 	public static double RadNormalize(double rad1)
 	{
-		
+		return (rad1 % (2*pi));
 	}
 	public static double powerResult(double base,double power)
 	{
+		double answer = 1;
+		try 
+		{
+			if(power==0)
+			{
+				return 1;
+			}
+			if(power<0)
+			{
+				throw new Exception("Negative!! Invalid!!");
+			}
+			else
+			{
+				for(int i=1;i<=power;i++)
+				{
+					answer = answer * base;
+				}
+			}
+			return answer;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	return answer;
 		
 	}
 	public static long Factorial(int den)
 	{
-		
+		long fact = 1;
+		try
+		{
+			if(den<0)
+			{
+				throw new Exception("Negative!!");
+			}
+			else
+			{
+				for(int i=1;i<=den;i++)
+				{
+					fact = fact * i;
+				}
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return fact;
 	}
 	public static double TrignoSin(double radinput)
 	{
 		/*
 		 * taylor series for sin is summation of (-1)n* x(2n+1)/ fact(2n+1)
 		 * */
+		sinResult = 0;
+		double calNormedAngle = RadNormalize(radinput);
 		
+		
+		for(int i=1;i<=10;i++)
+		{
+			
+			
+			sinResult = sinResult + (powerResult(-1, i-1) * powerResult(calNormedAngle, (2*i)-1) / Factorial((2*i)-1));
+		}
+		
+		return sinResult;
 	}
 	public static double TrignoCos()
 	{
