c876090675d9751061af21918dcc2248ae64f80b
Kalpit Soni <31398432+sonikalpit@users.noreply.github.com>
2020-11-10 12:14:01 +0530

    Version 2.0
    Implemented Power, Factorial and Sin method to test whether those results are accurate with Math's functions.

--- TrignoFunctions/src/TrigoTaylorFunctionTest.java
+++ TrignoFunctions/src/TrigoTaylorFunctionTest.java
@@ -4,20 +4,21 @@ import org.junit.Test;
 
 public class TrigoTaylorFunctionTest {
 
+	double delta = 0.0001;
 	
 	@Test
 	public void testPowerResult() {
-		assertEquals(Math.pow(2, 3), TrigoTaylorFunction.powerResult(2, 3));
+		assertEquals(Math.pow(2, 3), TrigoTaylorFunction.powerResult(2, 3),delta);
 	}
 
 	@Test
 	public void testFactorial() {
-		fail("Not yet implemented");
+		assertEquals(120, TrigoTaylorFunction.Factorial(5),delta);
 	}
 
 	@Test
 	public void testTrignoSin() {
-		fail("Not yet implemented");
+		assertEquals(Math.sin(2), TrigoTaylorFunction.TrignoSin(2),delta);
 	}
 
 	@Test
