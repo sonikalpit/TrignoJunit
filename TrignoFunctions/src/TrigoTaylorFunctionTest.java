d0c1bffee5c843221cf886b055d20103b4a4fc07
Kalpit Soni <31398432+sonikalpit@users.noreply.github.com>
2020-11-10 11:58:04 +0530

    Version 1.0
    Initial Junit Test for Sin, Cos, Tan

---
+++ TrignoFunctions/src/TrigoTaylorFunctionTest.java
@@ -0,0 +1,49 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TrigoTaylorFunctionTest {
+
+	
+	@Test
+	public void testPowerResult() {
+		assertEquals(Math.pow(2, 3), TrigoTaylorFunction.powerResult(2, 3));
+	}
+
+	@Test
+	public void testFactorial() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTrignoSin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTrignoCos() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testTrignoTan() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testTrignoSin1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTrignoCos1() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testTrignoTan1() {
+		fail("Not yet implemented");
+	}
+	
+
+}
